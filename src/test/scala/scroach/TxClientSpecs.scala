a16012015eb79713d57478f313c4f536c907e58f
Philippe Laflamme <philippe.laflamme@gmail.com>
2015-01-25 15:22:05 -0500

    Adds test for tx abort / reset.

--- src/test/scala/scroach/TxClientSpecs.scala
+++ src/test/scala/scroach/TxClientSpecs.scala
@@ -41,4 +41,37 @@ class TxClientSpecs extends ScroachSpec {
       case _: UnsupportedOperationException => ()
     }
   }
+
+  it should "reset tx on abort" in {
+    val kv = new TestKv {
+      private[this] var first = true
+      override val endTxEndpoint = Service.mk { (req: EndTransactionRequest) =>
+        req.header.txn match {
+          case Some(txn) => {
+            txn.id should be ('empty)
+            if(first) {
+              first = false
+              val respTx = txn.copy(id = Some(ByteString.copyFromUtf8("tx-id")), timestamp = Some(Timestamp(10, 0)), priority = Some(4))
+              Future.value(EndTransactionResponse(header = ResponseHeader(error = Some(Error(transactionAborted = Some(TransactionAbortedError(txn = respTx)))), txn = Some(respTx))))
+            } else {
+              // Client is expected to use the provided tx priority for future transactions
+              txn.priority should be(Some(4))
+              val respTx = txn.copy(id = Some(ByteString.copyFromUtf8("tx-id2")), timestamp = Some(Timestamp(10, 0)))
+              Future.value(EndTransactionResponse(header = ResponseHeader(txn = Some(respTx))))
+            }
+          }
+          case None => Future.exception(new IllegalStateException())
+        }
+      }
+    }
+
+    var tries = 0
+    Await.result {
+      KvClient(kv, "root").tx() { txClient =>
+        tries = tries + 1
+        Future.Done
+      }
+    }
+    tries should be(2)
+  }
 }
