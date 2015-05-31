d3fcc94f00180fa26cae764491c7396cf094f6e7
Philippe Laflamme <philippe.laflamme@gmail.com>
2015-05-31 11:19:14 -0400

    Use official proto files from cockroach-proto

---
+++ update-proto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CLONE_DIR="target/cockroachdb-proto"
+
+if [ ! -e target/cockroachdb-proto ];
+then
+  git clone https://github.com/cockroachdb/cockroach-proto.git $CLONE_DIR
+fi
+
+cd $CLONE_DIR && git reset --hard 943eeb7534a66d5a2d6ace238738699ab9b5264e && cd -
+
+rsync -vv -a --delete $CLONE_DIR/cockroach/proto/ src/main/protobuf/
