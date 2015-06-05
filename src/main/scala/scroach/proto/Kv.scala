e374db8c78a5389456841cc4adca203c32991b42
Philippe Laflamme <philippe.laflamme@gmail.com>
2015-06-05 19:22:01 -0400

    Improve the logging filter

--- src/main/scala/scroach/proto/Kv.scala
+++ src/main/scala/scroach/proto/Kv.scala
@@ -7,6 +7,7 @@ import cockroach.proto._
 import com.trueaccord.scalapb.GeneratedMessage
 import com.twitter.finagle.{Filter, SimpleFilter, Service}
 import com.twitter.finagle.httpx.{RequestBuilder, Response, Request}
+import com.twitter.util.{Duration, Stopwatch, Return, Throw}
 
 trait Kv {
   val containsEndpoint: Service[ContainsRequest, ContainsResponse]
@@ -95,16 +96,80 @@ case class TxKv(kv: Kv, name: String = util.Random.alphanumeric.take(20).mkStrin
  */
 case class HttpKv(client: Service[Request, Response]) extends Kv {
 
-  private[this] case class LoggingFilter[Req, Res]() extends SimpleFilter[Req,Res] {
+  private[this] case class LoggingFilter[Req <: GeneratedMessage <% CockroachRequest[Req], Res <: GeneratedMessage <% CockroachResponse[Res]](cmd: String) extends SimpleFilter[Req,Res] {
+    implicit class OptionLogging[T](opt: Option[T]) {
+      def toLog: String = opt.fold("") { _.toString }
+    }
+    implicit class TimestampLogging(opt: Option[Timestamp]) {
+      def toLog: String = opt.fold("") { ts => s"${ts.getWallTime},${ts.getLogical}" }
+    }
+    def txnString(transaction: Option[Transaction]) = {
+      for {
+        txn <- transaction
+        name <- txn.name
+        priority = txn.priority.toLog
+        iso = txn.isolation.toLog
+        state = txn.status.toLog
+        epoch = txn.epoch.toLog
+        ts = txn.timestamp.toLog
+        orig = txn.origTimestamp.toLog
+        max = txn.maxTimestamp.toLog
+      } yield s"(tx=$name isolation=$iso pri=$priority state=$state epoch=$epoch ts=$ts orig=$orig max=$max) "
+    } getOrElse("")
+
+    def errString(error: Option[Error]) = {
+      for {
+        err <- error
+        msg = err.message.toLog
+        retry = err.retryable.toLog
+        restart = err.transactionRestart.toLog
+        detail <- err.detail
+        typ = detail.value match {
+          case ErrorDetail.Value.Empty => "Unknown"
+          case _: ErrorDetail.Value.ConditionFailed => "ConditionFailed"
+          case _: ErrorDetail.Value.LeaseRejected => "LeaseRejected"
+          case _: ErrorDetail.Value.NotLeader => "NotLeader"
+          case _: ErrorDetail.Value.OpRequiresTxn => "OpRequiresTxn"
+          case _: ErrorDetail.Value.RangeKeyMismatch => "RangeKeyMismatch"
+          case _: ErrorDetail.Value.RangeNotFound => "RangeNotFound"
+          case _: ErrorDetail.Value.ReadWithinUncertaintyInterval => "ReadWithinUncertaintyInterval"
+          case _: ErrorDetail.Value.TransactionAborted => "TransactionAborted"
+          case _: ErrorDetail.Value.TransactionPush => "TransactionPush"
+          case _: ErrorDetail.Value.TransactionRetry => "TransactionRetry"
+          case _: ErrorDetail.Value.TransactionStatus => "TransactionStatus"
+          case _: ErrorDetail.Value.WriteIntent => "WriteIntent"
+          case _: ErrorDetail.Value.WriteTooOld => "WriteTooOld"
+        }
+      } yield s"(error=$typ retry=$retry restart=$restart) "
+    } getOrElse("ok")
+
+    def logRequest(nonce: String, request: Req) = {
+      val txStr = txnString(request.header.txn)
+      println(f"[$nonce]         ->$cmd%-14s $txStr")
+    }
+
+    def logResponse(nonce: String, response: Res, duration: Duration) = {
+      val tx = txnString(response.header.txn)
+      val err = errString(response.header.error)
+      println(f"[$nonce] ${duration.inMillis}%5dms <-$cmd%-14s $tx$err")
+    }
+
+    def logException(nonce: String, exception: Throwable, duration: Duration) = {
+      println(f"[$nonce] ${duration.inMillis}%5d <-$cmd%-14s ${exception.getMessage}")
+    }
+
     def apply(req: Req, service: Service[Req,Res]) = {
-      println(s"req == $req")
+      val nonce = scala.util.Random.alphanumeric.take(8).mkString
+      val watch = Stopwatch.start()
+      logRequest(nonce, req)
       service(req)
-        .respond { res =>
-          println(s"res == $res")
+        .respond {
+          case Return(res) => logResponse(nonce, res, watch())
+          case Throw(e) => logException(nonce, e, watch())
         }
     }
   }
-  private[this] case class ProtobufFilter[Req <: GeneratedMessage, Res <: GeneratedMessage <% CockroachResponse[Res]](cmd: String, parse: (InputStream) => Res) extends Filter[Req, Res, Request, Response] {
+  private[this] case class ProtobufFilter[Req <: GeneratedMessage <% CockroachRequest[Req], Res <: GeneratedMessage <% CockroachResponse[Res]](cmd: String, parse: (InputStream) => Res) extends Filter[Req, Res, Request, Response] {
     def apply(req: Req, service: Service[Request, Response]) = {
       val request = RequestBuilder
         .create()
@@ -132,8 +197,8 @@ case class HttpKv(client: Service[Request, Response]) extends Kv {
   val batchEndpoint = newEndpoint[BatchRequest, BatchResponse]("Batch", BatchResponse.parseFrom)
   val endTxEndpoint = newEndpoint[EndTransactionRequest, EndTransactionResponse]("EndTransaction", EndTransactionResponse.parseFrom)
 
-  private[this] def newEndpoint[Req <: GeneratedMessage, Res <: GeneratedMessage <% CockroachResponse[Res]](name: String, parseFrom: (InputStream) => Res): Service[Req, Res] = {
+  private[this] def newEndpoint[Req <: GeneratedMessage <% CockroachRequest[Req], Res <: GeneratedMessage <% CockroachResponse[Res]](name: String, parseFrom: (InputStream) => Res): Service[Req, Res] = {
     // TODO: retrying semantics for 429 and possibly 500s
-    /* LoggingFilter[Req, Res] andThen */ ProtobufFilter[Req, Res](name, parseFrom) andThen client
+    LoggingFilter[Req, Res](name) andThen ProtobufFilter[Req, Res](name, parseFrom) andThen client
   }
 }
