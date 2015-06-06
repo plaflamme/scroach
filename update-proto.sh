14cf61fd0f95c39171e942abea5f91b9528b8264
Philippe Laflamme <philippe.laflamme@gmail.com>
2015-06-06 10:25:25 -0400

    Update cockroach-proto

--- update-proto.sh
+++ update-proto.sh
@@ -7,6 +7,7 @@ then
   git clone https://github.com/cockroachdb/cockroach-proto.git $CLONE_DIR
 fi
 
-cd $CLONE_DIR && git reset --hard 943eeb7534a66d5a2d6ace238738699ab9b5264e && cd -
+cd $CLONE_DIR && git reset --hard $1 && cd -
 
 rsync -vv -a --delete $CLONE_DIR/ src/main/protobuf/
+echo $1 > src/main/protobuf/VERSION
